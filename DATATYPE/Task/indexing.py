6cfac0ed7b4c6278d9bfa1eb09388f18a21db9dc
Arun silawat <arunsilawat722362@gmail.com>
2024-06-19 15:58:02 +0530

    Python

---
+++ DATATYPE/Task/indexing.py
@@ -0,0 +1,17 @@
+# mystr="ArunSilawat"
+# print(mystr.index("n"))
+# print(mystr.index("n",2))
+# print(mystr.index("n",2,5))
+# print(mystr.index("A",2,5))
+# print(mystr[0])
+# print(mystr[-1])
+
+#----------------------------------------------------------------------
+
+x=[3,44,5,5655,544,34,34]
+print(x.index(5))
+print(type(x))
+
+x=(3,44,5,5655,544,34,34)
+print(x.index(5))
+print(type(x))
