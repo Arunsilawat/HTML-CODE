a8f046e00fcd8828684a72226d76140f2ba8efe2
Arun silawat <arunsilawat722362@gmail.com>
2024-06-22 14:29:49 +0530

    method

--- DATATYPE/string.py
+++ DATATYPE/string.py
@@ -1,6 +1,20 @@
-my_str='Neeraj'
+# my_str='Neeraj'
 
-my_str[0]=n
+# my_str[0]=n
+# --------------------------------------------------------------------------------------
 
+# mystr="Arun Silawat"
+# print(len(mystr))  #length nikalte he
+
+# print(max(mystr)) 
+# print(min(mystr)) 
+# print(type(mystr)) 
+# # print(ord.max((mystr)))
+# print(id(mystr))  
+
+
+a="123"
+b="abc"
+print(a.join(b))
 
 
