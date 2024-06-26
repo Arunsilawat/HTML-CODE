85460c7dc7c7e19b32e17bad6082e21dc2661585
Arun silawat <arunsilawat722362@gmail.com>
2024-06-26 14:03:15 +0530

    Tuple topic

---
+++ DATATYPE/tuple.py
@@ -0,0 +1,12 @@
+
+
+my_tuple=(10,20,30,40)
+print(len(my_tuple))
+print(max(my_tuple))
+print(min(my_tuple))
+print(sum(my_tuple))
+print(type(my_tuple))
+print(list(my_tuple))
+print(my_tuple.count(40))
+print(my_tuple.index(40))
+print(tuple(my_tuple))
