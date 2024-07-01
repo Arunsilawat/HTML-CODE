209acaa8ee39682b76114ddd3eceeb450a9554a5
Arun silawat <arunsilawat722362@gmail.com>
2024-07-01 14:42:03 +0530

    Control Flow

---
+++ Control Flow/Control_flow.py
@@ -0,0 +1,16 @@
+#1Sequantial  --> line by line
+#2->Condition --> if if else,if elif,if elif elif.
+#3->transfar  --> Comtinuew,pass,breack
+#4->iteretive --> for,while
+#-------------------if------------------------
+n=int(input("Enter your age"))
+if n>=18:print("You are eligible")
+print("Thank you for visit")
+
+#------------------if else -----------------------
+n=int(input("Enter your age"))
+if n>=18:
+    print("You are eligible")
+else:
+    print("You are not eligible")
+print("Thank you for visit")
