0e119cb8d763f668b35016114d649995b14bdb05
Arun silawat <arunsilawat722362@gmail.com>
2024-07-03 15:03:37 +0530

    while loop

--- Control Flow/Control_flow.py
+++ Control Flow/Control_flow.py
@@ -30,13 +30,84 @@
 #     my_list.append(x)
 # print(my_list)
 
-my_list1=input("Enter Name")
-v,c=0,0
-for i in my_list1:
-    if i=='a' or i=='i' or i=='o' or i=='u' or i=='e':
-        v=v+1
-    else:
-        c=c+1
-print(v)
-print(c)
-    
+# my_list1=input("Enter Name")
+# v,c=0,0
+# for i in my_list1:
+#     if i=='a' or i=='i' or i=='o' or i=='u' or i=='e':
+#         v=v+1
+#     else:
+#         c=c+1
+# print(v)
+# print(c)
+#----------------------------------------------------------------------
+# n=int(input("Enter NUmber :"))
+# i=1
+# sum=0
+# while i<=n:
+#     sum=sum+i
+#       if i<=n-1:
+#         print(i,end=",")
+#       else:
+#         print(i)
+#       i=i+1
+#---------------------------------------------
+#-------sum --------------
+# n=int(input("Enter NUmber :"))
+# i=1
+# sum=0
+# while i<=n:
+#       sum=sum+i
+#       if i<=n-1:
+#         print(i,end="+")
+#       else:
+#         print(i,end="=")
+#       i=i+1
+# print(sum)
+
+#-------------------------------------------------------------------
+n=int (input("Enter no."))
+i=1
+sum=0
+while i<=n:
+     if i==n:
+         print(i,end="=")
+     else:
+         print(i,end="+")
+         sum=sum+i
+     i=i+1
+print(sum)
+# ==============Even No================
+
+n=int (input("Enter no."))
+i=1
+sum=0
+while i<=n:
+     if i%2==0:
+         print(i,end=",") 
+     i=i+1
+# ============ Factorial =================
+n=int (input("Enter no."))
+i=1
+multi=1
+while i<=n:
+     multi=multi*i
+     if i<n:
+         print(i,end="=") 
+     else:
+         print(i,end="x")
+         
+     i=i+1
+print(multi)
+
+n=int (input("Enter no."))
+i=1
+multi=1
+while i<=n:
+     multi=multi*n
+     if i<n:
+         print(n,end="=") 
+     else:
+         print(n,end="x")
+         
+     n=n-1
+print(multi)
