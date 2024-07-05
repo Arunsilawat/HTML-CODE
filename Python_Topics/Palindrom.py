fab12e9e97ba5e218872c50ebdf17dab06fff789
Arun silawat <arunsilawat722362@gmail.com>
2024-07-05 16:43:14 +0530

    pattern

---
+++ Python_Topics/Palindrom.py
@@ -0,0 +1,22 @@
+# n=input("Enter Value :")
+# m=n[ : :-1]
+# if n==m:
+#     print("Palindrom")
+# else:
+#     print("Not Palindrom")
+
+# -----------------------------------------------------------------
+# n=int(input("Enter Value :"))
+# digit,x=0,n
+# while n>0:
+#     rev=n%10
+#     digit=digit*10+rev
+#     n=n//10
+# print(digit)
+# print(n)
+# print(x)
+# if x==digit:
+#     print("Palindrom")
+# else:
+#     print("Not a Palindrom")
+#--------------------------------------------------------------------
