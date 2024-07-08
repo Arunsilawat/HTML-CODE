21882f0ef6afdb277ff330cb4301ecda66fa2e60
Arun silawat <arunsilawat722362@gmail.com>
2024-07-08 14:26:43 +0530

    Function

---
+++ Function/function.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x-y,x/y,x*y,x%y,x//y
+    # return (a-b-c)
+# p=int(input("Enter number"))
+# q=int(input("Enter number"))
+# r=int(input("Enter number"))
+# x=add(p,q,r)
+# print(x)
+# x=x-100
+# print(x)
+# --------------------
+g=int(input("Enter number"))
+h=int(input("Enter number"))
+a,b,c,d,e=add(g,h)
+print(a,b,c,d,e)
