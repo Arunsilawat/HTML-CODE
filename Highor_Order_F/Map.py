1c6eecda29aa1075ce8e4035876ca658d4ddb322
Arun silawat <arunsilawat722362@gmail.com>
2024-07-12 14:15:40 +0530

    Add numbers

--- Highor_Order_F/Map.py
+++ Highor_Order_F/Map.py
@@ -1,7 +1,15 @@
 #map() -->number or input == no of output-->colection return aayega
 
-my_list=[1,2,3,4,5]
-def squer(n):
-    return n**2
-new_list=list(map(squer,my_list))
+# my_list=[1,2,3,4,5]
+# def squer(n):
+#     return n**2
+# new_list=list(map(squer,my_list))
+# print(new_list)
+
+
+my_list1=[1,2,3,4,5]
+my_list2=[5,4,3,2,1]
+def add(x,y):
+    return x+y
+new_list=list(map(add,my_list1,my_list2))
 print(new_list)
