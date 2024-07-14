ffc5694b693b9eb0aeedfdd59c96118fb539d113
Arun silawat <arunsilawat722362@gmail.com>
2024-07-14 20:02:35 +0530

    Typecasting

---
+++ Control Flow/Type_Casting.py
@@ -0,0 +1,15 @@
+# collection=(12,33,43,23)
+# print(collection)
+# print(type(collection))
+# x=(collection)
+# print(type(x))
+# print(type(int(collection)))
+my_list=[23,433,432,44,533]
+x=tuple(my_list)
+print(x)
+
+Number=12
+print(ord("A"))
+print(chr(65))
+print(oct(33))
+print(hex(65))
