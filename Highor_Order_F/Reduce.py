a6e64a87cba1c9f4e95f034849bb7e09da30e278
Arun silawat <arunsilawat722362@gmail.com>
2024-07-16 14:32:56 +0530

    Reduce

--- Highor_Order_F/Reduce.py
+++ Highor_Order_F/Reduce.py
@@ -9,7 +9,7 @@ def max_digit(x,y):
 print(functools.reduce(max_digit,my_list))
 
 #--------------------------------------------------------------
-form functools import reduce   #only reduce ko import kiya
+form functools import reduce   #only reduce ko import kiya   only single objet hi return aayega
 my_list=[10,23,43,22,65]
 def max_digit(x,y):
     if x>y:
@@ -33,5 +33,26 @@ print(reduce(min_digit,my_list))
 my_list=[10,23,43,22,65]
 def sum_digit(x,y):
         return x+y
+    
+print(reduce(sum_digit,my_list))
+
+#--------------smallest nu --------------------------
+from functools import reduce
+my_list=[10,23,43,22,65,7]
+def sum_digit(x,y):
+        if x<y:
+            return x
+        else:
+            return y
+    
+print(reduce(sum_digit,my_list))
+#---------------- highest ---------------------------
+from functools import reduce
+my_list=[10,23,43,22,65,7]
+def sum_digit(x,y):
+        if x>y:
+            return x
+        else:
+            return y
     
 print(reduce(sum_digit,my_list))
