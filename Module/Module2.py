9ad42ae00ad0bd4121d6f9ff560745d5eda89839
Arun silawat <arunsilawat722362@gmail.com>
2024-07-23 14:25:20 +0530

    Module

---
+++ Module/Module2.py
@@ -0,0 +1,8 @@
+from Module1 import add as a,sub as s  #Module1 se import karaya he function and variable
+                                       #eliyas(add as a) se function ke bade naame ko sort karte h
+x=50                                    #__init__.py se peceg banega | 
+y=20
+p=a(x,y)
+print(p)
+s=s(x,y)
+print(s)
