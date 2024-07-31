377ae9ac882fa6524a2e7a8ad65e9db51f0cb0a5
Arun silawat <arunsilawat722362@gmail.com>
2024-07-31 14:30:20 +0530

    Class With Static Class Varible

--- Class/Static_class_variable.py
+++ Class/Static_class_variable.py
@@ -12,13 +12,15 @@ class Student:
         print("School Name :",Student.school)
         print("Center Code",Student.centercode)
         print("Great :",Student.great)
+ 
 
 obj=Student("Arun",1001)
 obj.display()
 print("Class Name se :",Student.school)
 print("Object Se :",obj.school)
-
-#------------------------------------------------------------
+obj1=Student("Ritu",1009)
+obj1.display()
+  
 print("--------------------------------------------------------------------------------------------------------")
 class Employee:
     Company="SS Creation"
@@ -33,5 +35,9 @@ class Employee:
         print("Age : ",self.age)
         print("Company Code : ",Employee.Company_Code)
         print("Company Project : ",Employee.Company_Project)
+        print("City : ",Employee.city)
 obj=Employee("Arun",24)
+
+Employee.city="Bhopal"
 obj.display()
+print("--------------------------------------------------------------------------------------------------------")
