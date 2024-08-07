eae80226bb3234b4551c5161872dc82606c55449
Arun silawat <arunsilawat722362@gmail.com>
2024-08-07 15:09:22 +0530

    Single and multi lavel inheritance

--- Class/Constractor.py
+++ Class/Constractor.py
@@ -1,18 +1,19 @@
-class Student:
-    x=10
-    y=20
-    def __init__(self,x,y):
-        print("this is from first Constructor")
-        print(id(self))
-    def __init__(self):
-        print("this is from second Constructor")    #last bala constractor hi run hoga 
-        print(id(self))
-    @staticmethod                             #
-    def display():
-        print("This is from Display")
-obj=Student()
-print(id(obj))
-print(obj.x)
-obj.display()
-obj1=Student()
-print(id(obj1))
+# class Student:
+#     x=10
+#     y=20
+#     def __init__(self,x,y):
+#         print("this is from first Constructor")
+#         print(id(self))
+#     def __init__(self):
+#         print("this is from second Constructor")    #last bala constractor hi run hoga 
+#         print(id(self))
+#     @staticmethod                             #
+#     def display():
+#         print("This is from Display")
+# obj=Student()
+# print(id(obj))
+# print(obj.x)
+# obj.display()
+# obj1=Student()
+# print(id(obj1))
+
--- Class/Properties/Inheritence.py
+++ Class/Properties/Inheritence.py
@@ -1,11 +1,43 @@
-class P1:
-    def display(self):
-        print("P1 class")
-class C1(P1):
-    def show(self):
-        print("C1 class")
+# class P1:
+#     def display(self):
+#         print("P1 class")
+# class C1(P1):
+#     def show(self):
+#         print("C1 class")
         
-        P1.display(self)
+#         P1.display(self)
+# obj=C1()
+# obj.display()
+# obj.show() 
+#------------------single lavel------------------------
+# class P:
+#     city="Bhopal"
+#     def display(self):
+#         print("This is from Display")
+# class C(P):
+#     def show(self):
+#         print("This is from Show")
+#         print("City :",P.city)
+# obj=C()
+# obj.show()
+# print(obj.city,obj.display())
+
+#------------------multi lavel------------------------
+class P:
+    city="Bhopal"
+    def display(self):
+        print("This is from Display")
+class C(P):
+    def show(self):
+        print("This is from Show")
+        print("City :",P.city)
+class C1(C):
+    city1=P.city
+    def show1(self):
+        (self.display())
+        (self.show())
 obj=C1()
-obj.display()
+print(obj.city1)
+obj.show1()
 obj.show()
+obj.display()
