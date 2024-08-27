4ccd5f201cc3f7aa3f33f0e1fc8fc3537dc1e9bc
Arun silawat <arunsilawat722362@gmail.com>
2024-08-27 15:52:28 +0530

    Palindrom

--- DATATYPE/lis.py
+++ DATATYPE/lis.py
@@ -8,3 +8,13 @@ for i in range(0,len(lis)):
     lis[i]=lis[i][0].upper()+lis[i][1:]  #first latter capital   
 print(lis)
 
+#---------------------------> palindrom <------------------------------------
+#question-->WAp to create a new list with the help list such that it contain 
+#only palindrom string which has a length more then 2.
+
+lis=["ababa","nitin","we","new","www","a"]
+lis1=[]
+for i in lis:
+    if i==i[ : :-1] and len(i)>2:
+        lis1.append(i)
+print(lis1)
