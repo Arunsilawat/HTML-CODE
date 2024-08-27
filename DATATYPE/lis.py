35040127a24ae4ed2c63064cddd355fd14d7110a
Arun silawat <arunsilawat722362@gmail.com>
2024-08-27 15:45:01 +0530

    Upper Latter

---
+++ DATATYPE/lis.py
@@ -0,0 +1,9 @@
+lis=["we","are","here","to","revise","python"]
+print(len(lis))
+# lis[0].capitalize()
+#capitalize karna 
+for i in range(0,len(lis)):
+    # lis[i]=lis[i].capitalize()         #first latter capital
+    # lis[i]=lis[i].upper()              #all latter capital
+    lis[i]=lis[i][0].upper()+lis[i][1:]  #first latter capital   
+print(lis)
