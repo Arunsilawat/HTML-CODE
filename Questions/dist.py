ce6d8b6db3a83deedcc0cc554cb7c96d58cf0ac7
Arun silawat <arunsilawat722362@gmail.com>
2024-08-29 15:07:39 +0530

    Dict

---
+++ Questions/dist.py
@@ -0,0 +1,8 @@
+dict1={123:["ajay",9000900909],
+       456:["vijay",674837483],
+       789:["sagar",909988666]
+       }
+dict1[789][1]=6666666666
+dict1[234]=["tarun",434343434]          #new value add krna
+dict1.update({890:["Arun",222222222]})  #new value add krna
+print(dict1)
