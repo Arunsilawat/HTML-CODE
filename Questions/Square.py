815a13f2e544db506e904f648f3aae94d9998c66
Arun silawat <arunsilawat722362@gmail.com>
2024-10-08 03:44:04 +0530

    Pattern

---
+++ Questions/Square.py
@@ -0,0 +1,8 @@
+def square(num):
+    print("Square : ",num*num)
+  
+while True:
+    a=int(input("Enter Num : "))
+    square(a)
+
+
---
+++ Questions/even_odd.py
@@ -0,0 +1,10 @@
+def evenodd(num):
+    if num%2==0:
+        print("Even Num")
+    else:
+        print("Odd Num")
+while True:
+    a=int(input("Enter Num : "))
+    evenodd(a)
+
+
